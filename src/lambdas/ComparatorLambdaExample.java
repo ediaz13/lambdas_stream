b81ed6ed5c986751f51611fc6bd8188cbae026c3
EMANUEL DIAZ <emadiaz@santandertecnologia.com.ar>
2021-12-31 08:25:19 -0300

    13 - Comparator using Lambda

---
+++ src/lambdas/ComparatorLambdaExample.java
@@ -0,0 +1,31 @@
+package lambdas;
+
+import java.util.Comparator;
+
+public class ComparatorLambdaExample {
+    public static void main(String[] args) {
+        /**
+         * Prior Java 8
+         */
+
+        Comparator<Integer> comparator = new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o1.compareTo(o2);
+                //0 o1==02
+                //1 o1>02
+                //-1 o1<02
+            }
+        };
+
+        System.out.println("Result of comparator is: " + comparator.compare(3, 2));
+
+        Comparator<Integer> comparatorLambda = (Integer a, Integer b) -> a.compareTo(b);
+
+        System.out.println("Result of comparatorLambda is: " + comparatorLambda.compare(3, 2));
+
+        Comparator<Integer> comparatorLambda1 = (a, b) -> a.compareTo(b);
+
+        System.out.println("Result of comparatorLambda1 is: " + comparatorLambda1.compare(3, 2));
+    }
+}
